8d0fdeedcf30547191f5e019066c049901d9e04a
adam <minge.adam@gmail.com>
2021-02-08 21:08:21 +0100

    implemented music node

--- include/egnim/engine/core/music_node.h
+++ include/egnim/engine/core/music_node.h
@@ -1,8 +1,53 @@
-//
-// Created by adam on 07.02.2021.
-//
-
 #ifndef MUSIC_NODE_H
 #define MUSIC_NODE_H
+
+/* ----------------------------------- SFML --------------------------------- */
+
+/* --------------------------------- Standard ------------------------------- */
+
+/* ----------------------------------- Local -------------------------------- */
+#include <egnim/engine/core/scene_node.h>
+#include <egnim/engine/core/context.h>
+/* -------------------------------------------------------------------------- */
+
+namespace core
+{
+
+  class MusicNode : public SceneNode
+  {
+  public:
+    struct Settings
+    {
+      float attenuation = 1.f;
+      float min_distance = 1.f;
+      float pitch = 1.f;
+      float volume = 100.f;
+    };
+
+  public:
+    explicit MusicNode(Context::musics_holder& musics_holder);
+    ~MusicNode() override;
+
+    void setDefaultSettings(const Settings& settings);
+    const Settings& getDefaultSettings() const;
+
+    bool play();
+    bool play(std::string_view id, bool loop = false);
+    bool play(std::string_view id, const Settings& settings, bool loop = false);
+
+    void stop();
+    void pause();
+
+    bool isPlaying();
+    bool isPaused();
+    bool isStopped();
+
+  private:
+    Context::musics_holder& m_musics_holder;
+    Settings m_default_settings;
+    sf::Music* m_current_music;
+  };
+
+}
 
 #endif //MUSIC_NODE_H
---
+++ src/egnim/engine/core/music_node.cpp
@@ -0,0 +1,85 @@
+/* ----------------------------------- Local -------------------------------- */
+#include <egnim/engine/core/music_node.h>
+/* -------------------------------------------------------------------------- */
+
+namespace core {
+
+MusicNode::MusicNode(Context::musics_holder& musics_holder) :
+  m_musics_holder(musics_holder),
+  m_default_settings(Settings{}),
+  m_current_music(nullptr)
+{
+
+}
+
+MusicNode::~MusicNode() = default;
+
+void MusicNode::setDefaultSettings(const Settings& settings)
+{
+  m_default_settings = settings;
+}
+
+const MusicNode::Settings& MusicNode::getDefaultSettings() const
+{
+  return m_default_settings;
+}
+
+bool MusicNode::play()
+{
+  if(!m_current_music)
+    return false;
+
+  m_current_music->play();
+  return true;
+}
+
+bool MusicNode::play(std::string_view id, bool loop)
+{
+  return play(id, m_default_settings, loop);
+}
+
+bool MusicNode::play(std::string_view id, const Settings& settings, bool loop)
+{
+  if (!m_musics_holder.contains(id))
+    return false;
+
+  m_current_music = &m_musics_holder.get(id);
+
+  m_current_music->setAttenuation(settings.attenuation);
+  m_current_music->setMinDistance(settings.min_distance);
+  m_current_music->setPitch(settings.pitch);
+  m_current_music->setVolume(settings.volume);
+  m_current_music->setLoop(loop);
+  m_current_music->play();
+
+  return true;
+}
+
+void MusicNode::stop()
+{
+  if(m_current_music)
+    m_current_music->stop();
+}
+
+void MusicNode::pause()
+{
+  if(m_current_music)
+    m_current_music->pause();
+}
+
+bool MusicNode::isPlaying()
+{
+  return m_current_music && m_current_music->getStatus() == sf::Music::Playing;
+}
+
+bool MusicNode::isPaused()
+{
+  return m_current_music && m_current_music->getStatus() == sf::Music::Paused;
+}
+
+bool MusicNode::isStopped()
+{
+  return m_current_music && m_current_music->getStatus() == sf::Music::Stopped;
+}
+
+} // namespace core
