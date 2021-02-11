60acc8ae09f49b6545e512082eaff6b90cafa264
adam <minge.adam@gmail.com>
2021-02-11 21:02:01 +0100

    added label node

--- include/egnim/engine/core/label_node.h
+++ include/egnim/engine/core/label_node.h
@@ -1,8 +1,51 @@
-//
-// Created by adam on 07.02.2021.
-//
-
 #ifndef LABEL_NODE_H
 #define LABEL_NODE_H
+
+/* ----------------------------------- SFML --------------------------------- */
+#include <SFML/Graphics/Font.hpp>
+#include <SFML/Graphics/Text.hpp>
+/* --------------------------------- Standard ------------------------------- */
+#include <string_view>
+/* ----------------------------------- Local -------------------------------- */
+#include <egnim/engine/core/scene_node.h>
+/* -------------------------------------------------------------------------- */
+
+namespace egnim::core
+{
+
+  class LabelNode : public SceneNode
+  {
+  public:
+    explicit LabelNode(const sf::Font& font, const sf::String& text);
+    ~LabelNode() override;
+
+    void setFont(const sf::Font& font);
+    void setText(const sf::String& text);
+    void setCharacterSize(unsigned size);
+    void setLineSpacing(float spacingFactory);
+    void setLetterSpacing(float spacingFactory);
+    void setStyle(sf::Uint32 style);
+    void setFillColor(const sf::Color& color);
+    void setOutlineColor(const sf::Color& color);
+    void setOutlineThickness(float thickness);
+
+    const sf::Font& getFont() const;
+    const sf::String& getText() const;
+    unsigned getCharacterSize() const;
+    float getLineSpacing() const;
+    float getLetterSpacing() const;
+    sf::Uint32 getStyle() const;
+    const sf::Color& getFillColor() const;
+    const sf::Color& getOutlineColor() const;
+    float getOutlineThickness() const;
+
+  protected:
+    void drawCurrent(sf::RenderTarget& target, sf::RenderStates states) const override;
+
+  private:
+    sf::Text m_label;
+  };
+
+} // namespace egnim::core
 
 #endif //LABEL_NODE_H
---
+++ src/egnim/engine/core/label_node.cpp
@@ -0,0 +1,113 @@
+/* ----------------------------------- SFML --------------------------------- */
+#include <SFML/Graphics/RenderTarget.hpp>
+/* ----------------------------------- Local -------------------------------- */
+#include <egnim/engine/core/label_node.h>
+/* -------------------------------------------------------------------------- */
+
+namespace egnim::core
+{
+
+LabelNode::LabelNode(const sf::Font &font, const sf::String &text)
+{
+  setFont(font);
+  setText(text);
+}
+
+LabelNode::~LabelNode() = default;
+
+void LabelNode::setFont(const sf::Font& font)
+{
+  m_label.setFont(font);
+}
+
+void LabelNode::setText(const sf::String& text)
+{
+  m_label.setString(text);
+}
+
+void LabelNode::setCharacterSize(unsigned size)
+{
+  m_label.setCharacterSize(size);
+}
+
+void LabelNode::setLineSpacing(float spacingFactory)
+{
+  m_label.setLineSpacing(spacingFactory);
+}
+
+void LabelNode::setLetterSpacing(float spacingFactory)
+{
+  m_label.setLetterSpacing(spacingFactory);
+}
+
+void LabelNode::setStyle(sf::Uint32 style)
+{
+  m_label.setStyle(style);
+}
+
+void LabelNode::setFillColor(const sf::Color& color)
+{
+  m_label.setFillColor(color);
+}
+
+void LabelNode::setOutlineColor(const sf::Color& color)
+{
+  m_label.setOutlineColor(color);
+}
+
+void LabelNode::setOutlineThickness(float thickness)
+{
+  m_label.setOutlineThickness(thickness);
+}
+
+const sf::Font& LabelNode::getFont() const
+{
+  return *m_label.getFont();
+}
+
+const sf::String& LabelNode::getText() const
+{
+  return m_label.getString();
+}
+
+unsigned LabelNode::getCharacterSize() const
+{
+  return m_label.getCharacterSize();
+}
+
+float LabelNode::getLineSpacing() const
+{
+  return m_label.getLineSpacing();
+}
+
+float LabelNode::getLetterSpacing() const
+{
+  return m_label.getLetterSpacing();
+}
+
+sf::Uint32 LabelNode::getStyle() const
+{
+  return m_label.getStyle();
+}
+
+const sf::Color& LabelNode::getFillColor() const
+{
+  return m_label.getFillColor();
+}
+
+const sf::Color& LabelNode::getOutlineColor() const
+{
+  return m_label.getOutlineColor();
+}
+
+float LabelNode::getOutlineThickness() const
+{
+  return m_label.getOutlineThickness();
+}
+
+void LabelNode::drawCurrent(sf::RenderTarget &target, sf::RenderStates states) const
+{
+  target.draw(m_label);
+}
+
+} // namespace egnim::core
