fad19ee627354f03895cf5049523cb04aa3c3dfe
adam <minge.adam@gmail.com>
2021-02-11 18:28:04 +0100

    changed lazy pointer

--- include/egnim/engine/core/lazy_unique_pointer.h
+++ include/egnim/engine/core/lazy_unique_pointer.h
@@ -4,22 +4,13 @@
 /* --------------------------------- Standard ------------------------------- */
 #include <memory>
 #include <functional>
+#include <type_traits>
 /* -------------------------------------------------------------------------- */
 
 namespace egnim::core
 {
+
   template<typename TYPE>
-  class DefaultCreator
-  {
-  public:
-    std::unique_ptr<TYPE> operator()() const;
-
-    DefaultCreator() = default;
-    DefaultCreator(DefaultCreator&&) noexcept = default;
-    DefaultCreator& operator=(DefaultCreator&&) noexcept = default;
-  };
-
-  template<typename TYPE, typename CREATOR = DefaultCreator<TYPE>>
   class LazyUniquePointer
   {
     template<typename MAKER_TYPE>
@@ -52,31 +43,26 @@ namespace egnim::core
     void init() const;
 
   private:
-    explicit LazyUniquePointer(CREATOR&& creator);
-    explicit LazyUniquePointer();
+    explicit LazyUniquePointer(std::function<std::unique_ptr<TYPE>()>&& creator);
 
     TYPE* getter();
     const TYPE* getter() const;
 
   private:
     mutable std::unique_ptr<TYPE> m_object;
-    mutable std::optional<CREATOR> m_creator;
+    mutable std::optional<std::function<std::unique_ptr<TYPE>()>> m_creator;
   };
-
-  /* ----------------------------- DefaultCreator<TYPE> ----------------------- */
-
-  template<typename TYPE>
-  std::unique_ptr<TYPE> DefaultCreator<TYPE>::operator()() const
-  {
-    return std::make_unique<TYPE>();
-  }
 
   /* ------------------------------- make_unique_lazy ------------------------- */
 
   template<typename MAKER_TYPE>
   auto make_unique_lazy()
   {
-    return LazyUniquePointer<MAKER_TYPE, DefaultCreator<MAKER_TYPE>>();
+    auto creator = std::function<std::unique_ptr<MAKER_TYPE>()>([](){
+      return std::make_unique<MAKER_TYPE>();
+    });
+
+    return LazyUniquePointer<MAKER_TYPE>(std::move(creator));
   }
 
   template<typename MAKER_TYPE, typename ...ARGS>
@@ -86,7 +72,7 @@ namespace egnim::core
       return std::make_unique<MAKER_TYPE>(std::forward<ARGS>(args)...);
     });
 
-    return LazyUniquePointer<MAKER_TYPE, decltype(creator)>(std::move(creator));
+    return LazyUniquePointer<MAKER_TYPE>(std::move(creator));
   }
 
   template<typename MAKER_TYPE, typename CALLABLE, typename ...ARGS,
@@ -95,17 +81,31 @@ namespace egnim::core
   {
     auto creator = std::function<std::unique_ptr<MAKER_TYPE>()>([&args..., &initializer](){
       auto object = std::make_unique<MAKER_TYPE>(std::forward<ARGS>(args)...);
-      std::forward<CALLABLE>(initializer)(*object);
+
+      static_assert(std::is_same_v<bool, decltype(std::declval<CALLABLE>())> ||
+                    std::is_same_v<void, decltype(std::declval<CALLABLE>())>,
+                    "Callable initializer returning type must be one of (bool, void)");
+
+      if constexpr (std::is_same_v<bool, decltype(std::declval<CALLABLE>())>)
+      {
+        if(!std::forward<CALLABLE>(initializer)(*object))
+          return nullptr;
+      }
+      else
+      {
+        std::forward<CALLABLE>(initializer)(*object);
+      }
+
       return object;
     });
 
-    return LazyUniquePointer<MAKER_TYPE, decltype(creator)>(std::move(creator));
+    return LazyUniquePointer<MAKER_TYPE>(std::move(creator));
   }
 
   /* ------------------------------ LazyPointer<TYPE> ------------------------- */
 
-  template<typename TYPE, typename CREATOR>
-  LazyUniquePointer<TYPE, CREATOR>::LazyUniquePointer(LazyUniquePointer<TYPE,CREATOR>&& other) noexcept
+  template<typename TYPE>
+  LazyUniquePointer<TYPE>::LazyUniquePointer(LazyUniquePointer<TYPE>&& other) noexcept
   {
     m_object = std::move(other.m_object);
     m_creator = std::move(other.m_creator);
@@ -114,8 +114,8 @@ namespace egnim::core
     other.m_creator.reset();
   }
 
-  template<typename TYPE, typename CREATOR>
-  LazyUniquePointer<TYPE, CREATOR>& LazyUniquePointer<TYPE, CREATOR>::operator=(LazyUniquePointer<TYPE,CREATOR>&& other) noexcept
+  template<typename TYPE>
+  LazyUniquePointer<TYPE>& LazyUniquePointer<TYPE>::operator=(LazyUniquePointer<TYPE>&& other) noexcept
   {
     m_object = std::move(other.m_object);
     m_creator = std::move(other.m_creator);
@@ -126,38 +126,38 @@ namespace egnim::core
     return *this;
   }
 
-  template<typename TYPE, typename CREATOR>
-  TYPE& LazyUniquePointer<TYPE, CREATOR>::operator*()
+  template<typename TYPE>
+  TYPE& LazyUniquePointer<TYPE>::operator*()
   {
     return *getter();
   }
 
-  template<typename TYPE, typename CREATOR>
-  const TYPE& LazyUniquePointer<TYPE, CREATOR>::operator*() const
+  template<typename TYPE>
+  const TYPE& LazyUniquePointer<TYPE>::operator*() const
   {
     return *getter();
   }
 
-  template<typename TYPE, typename CREATOR>
-  TYPE* LazyUniquePointer<TYPE, CREATOR>::operator->()
+  template<typename TYPE>
+  TYPE* LazyUniquePointer<TYPE>::operator->()
   {
     return getter();
   }
 
-  template<typename TYPE, typename CREATOR>
-  const TYPE* LazyUniquePointer<TYPE, CREATOR>::operator->() const
+  template<typename TYPE>
+  const TYPE* LazyUniquePointer<TYPE>::operator->() const
   {
     return getter();
   }
 
-  template<typename TYPE, typename CREATOR>
-  LazyUniquePointer<TYPE, CREATOR>::operator bool() const
+  template<typename TYPE>
+  LazyUniquePointer<TYPE>::operator bool() const
   {
     return static_cast<bool>(getter());
   }
 
-  template<typename TYPE, typename CREATOR>
-  void LazyUniquePointer<TYPE, CREATOR>::init() const
+  template<typename TYPE>
+  void LazyUniquePointer<TYPE>::init() const
   {
     if(!m_object)
     {
@@ -166,36 +166,29 @@ namespace egnim::core
     }
   }
 
-  template<typename TYPE, typename CREATOR>
-  LazyUniquePointer<TYPE, CREATOR>::LazyUniquePointer(CREATOR&& creator) :
-    m_creator(std::forward<CREATOR>(creator))
+  template<typename TYPE>
+  LazyUniquePointer<TYPE>::LazyUniquePointer(std::function<std::unique_ptr<TYPE>()>&& creator) :
+    m_creator(std::move(creator))
   {
 
   }
 
-  template<typename TYPE, typename CREATOR>
-  LazyUniquePointer<TYPE, CREATOR>::LazyUniquePointer() :
-    m_creator(CREATOR{})
-  {
-
-  }
-
-  template<typename TYPE, typename CREATOR>
-  LazyUniquePointer<TYPE, CREATOR>::LazyUniquePointer(std::nullptr_t) :
+  template<typename TYPE>
+  LazyUniquePointer<TYPE>::LazyUniquePointer(std::nullptr_t) :
     m_creator(std::nullopt)
   {
 
   }
 
-  template<typename TYPE, typename CREATOR>
-  TYPE* LazyUniquePointer<TYPE, CREATOR>::getter()
+  template<typename TYPE>
+  TYPE* LazyUniquePointer<TYPE>::getter()
   {
     init();
     return m_object.get();
   }
 
-  template<typename TYPE, typename CREATOR>
-  const TYPE* LazyUniquePointer<TYPE, CREATOR>::getter() const
+  template<typename TYPE>
+  const TYPE* LazyUniquePointer<TYPE>::getter() const
   {
     init();
     return m_object.get();
