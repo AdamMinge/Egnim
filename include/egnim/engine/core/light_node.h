969c7f9a1acdf79206f753aa6eaf9f76d59c6b70
adam <minge.adam@gmail.com>
2021-02-14 08:33:05 +0100

    prepare light node to impl

--- include/egnim/engine/core/light_node.h
+++ include/egnim/engine/core/light_node.h
@@ -1,8 +1,24 @@
-//
-// Created by adam on 07.02.2021.
-//
-
 #ifndef LIGHT_NODE_H
 #define LIGHT_NODE_H
+
+/* ----------------------------------- SFML --------------------------------- */
+#include <SFML/Graphics/Texture.hpp>
+#include <SFML/Graphics/Sprite.hpp>
+/* ----------------------------------- Local -------------------------------- */
+#include <egnim/engine/core/node.h>
+/* -------------------------------------------------------------------------- */
+
+namespace egnim::core
+{
+
+  class LightNode : public Node
+  {
+  public:
+    explicit LightNode();
+    ~LightNode() override;
+
+  };
+
+} // namespace egnim::core
 
 #endif //LIGHT_NODE_H
---
+++ src/egnim/engine/core/light_node.cpp
@@ -0,0 +1,13 @@
+/* ----------------------------------- SFML --------------------------------- */
+#include <SFML/Graphics/RenderTarget.hpp>
+/* ----------------------------------- Local -------------------------------- */
+#include <egnim/engine/core/light_node.h>
+/* -------------------------------------------------------------------------- */
+
+namespace egnim::core {
+
+LightNode::LightNode() = default;
+
+LightNode::~LightNode() = default;
+
+} // namespace egnim::core
