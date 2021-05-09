cbb44420435c6667059dc29e577f7ee415a89343
adam <minge.adam@gmail.com>
2021-05-09 21:07:24 +0200

    created physics demo first scene ( multiple camera with box creation )

--- examples/physics_scenes/include/boxes_state.h
+++ examples/physics_scenes/include/boxes_state.h
@@ -2,8 +2,14 @@
 #define BOXES_STATE_H
 
 /* ----------------------------------- egnim -------------------------------- */
+#include <egnim/engine/events/keyboard_event_listener.h>
 #include <egnim/engine/events/mouse_event_listener.h>
+#include <egnim/engine/events/keyboard_event.h>
 #include <egnim/engine/events/mouse_event.h>
+#include <egnim/engine/physics/physics_material.h>
+#include <egnim/engine/physics/physics_body.h>
+#include <egnim/engine/scene/sprite_node.h>
+#include <egnim/engine/scene/camera_node.h>
 /* ----------------------------------- egnim -------------------------------- */
 #include <egnim/engine/scene/scene_state.h>
 /* -------------------------------------------------------------------------- */
@@ -17,15 +23,23 @@ public:
 private:
   void createBox(const sf::Vector2f& position);
   void createFloor(const sf::Vector2f& position);
+  void createCamera(const sf::Vector2f& position, const sf::FloatRect& rect, std::string_view name);
+
+  std::unique_ptr<egnim::scene::SpriteNode> createSpriteNode(const sf::Vector2f& position,
+                                                             const sf::Texture& texture,
+                                                             const egnim::physics::PhysicsMaterial& material,
+                                                             egnim::physics::PhysicsBody::Type type);
 
 private:
-  void mouseButtonPressed(const egnim::events::MouseButtonPressedEvent& pressed_event);
+  void mouseButtonPressed(const egnim::events::MouseButtonPressedEvent& pressed_button_event);
+  void keyboardKeyPressed(const egnim::events::KeyboardKeyPressedEvent& pressed_key_event);
 
   void onActive() override;
   void onInactive() override;
 
 private:
   egnim::events::MouseEventListener m_mouse_listener;
+  egnim::events::KeyboardEventListener m_keyboard_listener;
 };
 
 #endif //BOXES_STATE_H
--- examples/physics_scenes/src/boxes_state.cpp
+++ examples/physics_scenes/src/boxes_state.cpp
@@ -5,7 +5,7 @@
 #include <egnim/engine/physics/physics_shape.h>
 #include <egnim/engine/events/event_dispatcher.h>
 #include <egnim/engine/scene/scene_node.h>
-#include <egnim/engine/scene/sprite_node.h>
+#include <egnim/engine/scene/camera_manager.h>
 /* ----------------------------------- Local -------------------------------- */
 #include "boxes_state.h"
 /* -------------------------------------------------------------------------- */
@@ -18,79 +18,97 @@ BoxesState::BoxesState(egnim::core::StateStack& state_stack, egnim::core::Contex
 
 BoxesState::~BoxesState() = default;
 
-void BoxesState::createBox(const sf::Vector2f& position)
+std::unique_ptr<egnim::scene::SpriteNode> BoxesState::createSpriteNode(const sf::Vector2f& position,
+  const sf::Texture& texture, const egnim::physics::PhysicsMaterial& material, egnim::physics::PhysicsBody::Type type)
 {
   // get needed values
   auto& scene_node = getSceneNode();
   auto& physics_world = scene_node.getPhysicsWorld();
-  auto& texture = scene_node.getContext().getTextureHolder().get("box");
 
-  // create sprite / physics_body / physics_shape for box
-  auto box = std::make_unique<egnim::scene::SpriteNode>();
-  auto physics_body = std::make_unique<egnim::physics::PhysicsBody>(physics_world,egnim::physics::PhysicsBody::Type::DynamicBody);
-  auto physics_shape = std::make_unique<egnim::physics::PhysicsShapeBox>(
-    sf::Vector2f(static_cast<float>(texture.getSize().x),
-                      static_cast<float>(texture.getSize().y)), sf::Vector2f(0,0));
-
-  // set shape properties
-  physics_shape->setDensity(1.f);
-  physics_shape->setFriction(0.7f);
-  physics_shape->setRestitution(0.0f);
-  physics_shape->setRestitutionThreshold(1.0f);
-
-  // set body properties
-  physics_body->attachPhysicsShape(std::move(physics_shape));
-
-  // set node properties
-  box->attachComponent(std::move(physics_body));
-  box->setTexture(texture);
-  box->setOrigin(static_cast<float>(texture.getSize().x) / 2.f, static_cast<float>(texture.getSize().y) / 2.f);
-  box->setPosition(position);
-
-  // attach node to scene
-  scene_node.attachChild(std::move(box));
-}
-
-void BoxesState::createFloor(const sf::Vector2f& position)
-{
-  // get needed values
-  auto& scene_node = getSceneNode();
-  auto& physics_world = scene_node.getPhysicsWorld();
-  auto& texture = scene_node.getContext().getTextureHolder().get("floor");
-
-  // create sprite / physics_body / physics_shape for box
-  auto floor = std::make_unique<egnim::scene::SpriteNode>();
-  auto physics_body = std::make_unique<egnim::physics::PhysicsBody>(physics_world,egnim::physics::PhysicsBody::Type::StaticBody);
+  // create sprite / physics_body / physics_shape for node
+  auto node = std::make_unique<egnim::scene::SpriteNode>();
+  auto physics_body = std::make_unique<egnim::physics::PhysicsBody>(physics_world,type);
   auto physics_shape = std::make_unique<egnim::physics::PhysicsShapeBox>(
     sf::Vector2f(static_cast<float>(texture.getSize().x),
                  static_cast<float>(texture.getSize().y)), sf::Vector2f(0,0));
 
-  // set shape properties
-  physics_shape->setDensity(0.f);
-  physics_shape->setFriction(0.2f);
-  physics_shape->setRestitution(0.0f);
-  physics_shape->setRestitutionThreshold(1.0f);
+  // set shape material
+  physics_shape->setPhysicsMaterial(material);
 
-  // set body properties
+  // attach shape to body and body to node
   physics_body->attachPhysicsShape(std::move(physics_shape));
+  node->attachComponent(std::move(physics_body));
 
   // set node properties
-  floor->attachComponent(std::move(physics_body));
-  floor->setTexture(texture);
-  floor->setOrigin(static_cast<float>(texture.getSize().x) / 2.f, static_cast<float>(texture.getSize().y) / 2.f);
-  floor->setPosition(position);
+  node->setTexture(texture);
+  node->setOrigin(static_cast<float>(texture.getSize().x) / 2.f, static_cast<float>(texture.getSize().y) / 2.f);
+  node->setPosition(position);
 
-  // attach node to scene
-  scene_node.attachChild(std::move(floor));
+  return node;
 }
 
-void BoxesState::mouseButtonPressed(const egnim::events::MouseButtonPressedEvent& pressed_event)
+void BoxesState::createBox(const sf::Vector2f& position)
 {
-  if(pressed_event.getButton() == sf::Mouse::Button::Left)
+  auto& scene_node = getSceneNode();
+  scene_node.attachChild(createSpriteNode(
+    position, scene_node.getContext().getTextureHolder().get("box"),
+    egnim::physics::PhysicsMaterial(1.0f, 0.0f, 0.7f, 1.0f),
+    egnim::physics::PhysicsBody::Type::DynamicBody
+    ));
+}
+
+void BoxesState::createFloor(const sf::Vector2f& position)
+{
+  auto& scene_node = getSceneNode();
+  scene_node.attachChild(createSpriteNode(
+    position, scene_node.getContext().getTextureHolder().get("floor"),
+    egnim::physics::PhysicsMaterial(0.0f, 0.0f, 0.2f, 1.0f),
+    egnim::physics::PhysicsBody::Type::StaticBody
+  ));
+}
+
+void BoxesState::createCamera(const sf::Vector2f& position, const sf::FloatRect& rect, std::string_view name)
+{
+  auto& scene_node = getSceneNode();
+
+  auto camera_node = std::make_unique<egnim::scene::CameraNode>();
+  camera_node->setSize(sf::Vector2f(800, 600));
+  camera_node->setPosition(position);
+  camera_node->setViewport(rect);
+  camera_node->setEnabled(true);
+  camera_node->setName(name);
+
+  scene_node.attachChild(std::move(camera_node));
+}
+
+void BoxesState::mouseButtonPressed(const egnim::events::MouseButtonPressedEvent& pressed_button_event)
+{
+  auto& scene_node = getSceneNode();
+
+  if(pressed_button_event.getButton() == sf::Mouse::Button::Left)
   {
-    createBox(sf::Vector2f(static_cast<float>(pressed_event.getPosition().x),
-                                  static_cast<float>(pressed_event.getPosition().y)));
+    auto& camera_manager = scene_node.getCameraManager();
+    auto coordinate = camera_manager.mapPixelToCoords(pressed_button_event.getPosition(),
+                                                      camera_manager.getCameraNode(pressed_button_event.getPosition()));
+
+    createBox(coordinate);
   }
+}
+
+void BoxesState::keyboardKeyPressed(const egnim::events::KeyboardKeyPressedEvent& pressed_key_event)
+{
+  auto& scene_node = getSceneNode();
+  auto camera_move = sf::Vector2f(0.0f, 0.0f);
+
+  if(pressed_key_event.getKey() == sf::Keyboard::Up) camera_move.y -= 10.0f;
+  if(pressed_key_event.getKey() == sf::Keyboard::Down) camera_move.y += 10.0f;
+  if(pressed_key_event.getKey() == sf::Keyboard::Left) camera_move.x -= 10.0f;
+  if(pressed_key_event.getKey() == sf::Keyboard::Right) camera_move.x += 10.0f;
+
+  if(pressed_key_event.isAltPressed())
+    scene_node.findChildByName("camera_1")->move(camera_move);
+  if(pressed_key_event.isControlPressed())
+    scene_node.findChildByName("camera_2")->move(camera_move);
 }
 
 void BoxesState::onActive()
@@ -105,12 +123,21 @@ void BoxesState::onActive()
   m_mouse_listener.setMouseButtonPressedEventCallback([this](auto&& event){ this->mouseButtonPressed(event); });
   getContext().getEventDispatcher().addEventListener(std::addressof(m_mouse_listener), -1);
 
+  m_keyboard_listener.setKeyboardKeyPressedEventCallback([this](auto&& event){ this->keyboardKeyPressed(event); });
+  getContext().getEventDispatcher().addEventListener(std::addressof(m_keyboard_listener), -1);
+
   // init scene
   auto& scene_node = getSceneNode();
   auto& physics_world = scene_node.getPhysicsWorld();
 
   physics_world.setGravity(sf::Vector2f(0, 9.8f));
+
   createFloor(sf::Vector2f(300, 500));
+
+  createCamera(sf::Vector2f(300, 500),
+               sf::FloatRect(0.0, 0.0, 0.5, 1.0),"camera_1");
+  createCamera(sf::Vector2f(300, 500),
+               sf::FloatRect(0.5, 0.0, 0.5, 1.0),"camera_2");
 }
 
 void BoxesState::onInactive()
@@ -121,6 +148,7 @@ void BoxesState::onInactive()
 
   // disconnect listener
   getContext().getEventDispatcher().removeEventListener(std::addressof(m_mouse_listener));
+  getContext().getEventDispatcher().removeEventListener(std::addressof(m_keyboard_listener));
 }
 
 
